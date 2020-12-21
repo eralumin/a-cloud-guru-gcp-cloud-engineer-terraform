49da63aaa729a81bb65eaf47716fd3b6a8722e75
Kevin Marilleau <kevin.marilleau@gmail.com>
2020-12-21 01:12:01 +0100

    set billing account

--- main.tf
+++ main.tf
@@ -2,4 +2,5 @@
 resource "google_project" "project" {
   name       = "A Cloud Guru - GCACE"
   project_id = "${var.uid_prefix}-acg-gcace"
+  billing_account = var.billing_account
 }
--- vars.tf
+++ vars.tf
@@ -2,6 +2,10 @@ variable "uid_prefix" {
   type = string
 }
 
+variable "billing_account" {
+  type = string
+}
+
 variable "region" {
     type = string
     default = "us-central1"
