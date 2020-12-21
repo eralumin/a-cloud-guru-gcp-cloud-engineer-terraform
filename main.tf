1959c4851d32640e061962e3c316bb4433293520
Kevin Marilleau <kevin.marilleau@gmail.com>
2020-12-21 01:25:02 +0100

    enable compute api

--- main.tf
+++ main.tf
@@ -4,3 +4,9 @@ resource "google_project" "project" {
   project_id = "${var.uid_prefix}-acg-gcace"
   billing_account = var.billing_account
 }
+
+resource "google_project_service" "services" {
+  for_each = toset(var.services)
+  project  = google_project.project.project_id
+  service  = each.value
+}
--- vars.tf
+++ vars.tf
@@ -6,6 +6,11 @@ variable "billing_account" {
   type = string
 }
 
+variable "services" {
+    type = list
+    default = ["compute.googleapis.com"]
+}
+
 variable "region" {
     type = string
     default = "us-central1"
