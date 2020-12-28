f582ebbf5761d63816b2471b0cf43aedbb8db890
Kevin Marilleau <kevin.marilleau@gmail.com>
2020-12-28 01:06:37 +0100

    fix firewall from solution

--- custom-mode-vpcs-challenge-lab/firewall.tf
+++ custom-mode-vpcs-challenge-lab/firewall.tf
@@ -1,16 +1,20 @@
-resource "google_compute_firewall" "allow-frontend-ping-ingress" {
+resource "google_compute_firewall" "allow-ssh" {
   project   = google_project.project.project_id
-  name      = "allow-frontend-ping-ingress"
+  name      = "allow-ssh"
   network   = google_compute_network.vpc_network.name
   direction = "INGRESS"
 
-  target_service_accounts = [google_service_account.frontend.email]
-  source_ranges           = ["0.0.0.0/0"]
+  target_tags   = ["open-ssh"]
+  source_ranges = ["0.0.0.0/0"]
 
   allow {
-    protocol = "icmp"
+    protocol = "tcp"
+    ports    = ["22"]
   }
+
+  priority = 500
 }
+
 resource "google_compute_firewall" "allow-backend-ping-ingress" {
   project   = google_project.project.project_id
   name      = "allow-backend-ping-ingress"
@@ -26,13 +30,15 @@ resource "google_compute_firewall" "allow-backend-ping-ingress" {
   allow {
     protocol = "icmp"
   }
+
+  priority = 800
 }
 
-resource "google_compute_firewall" "deny-all-backend-egress" {
+resource "google_compute_firewall" "deny-backend-ingress" {
   project   = google_project.project.project_id
-  name      = "deny-all-backend-egress"
+  name      = "deny-backend-ingress"
   network   = google_compute_network.vpc_network.name
-  direction = "EGRESS"
+  direction = "INGRESS"
 
   target_service_accounts = [google_service_account.backend.email]
 
@@ -41,9 +47,23 @@ resource "google_compute_firewall" "deny-all-backend-egress" {
   }
 }
 
-resource "google_compute_firewall" "allow-backend-ping-backend-egress" {
+resource "google_compute_firewall" "allow-frontend-ping-ingress" {
   project   = google_project.project.project_id
-  name      = "allow-backend-ping-backend-egress"
+  name      = "allow-frontend-ping-ingress"
+  network   = google_compute_network.vpc_network.name
+  direction = "INGRESS"
+
+  target_service_accounts = [google_service_account.frontend.email]
+  source_ranges           = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "icmp"
+  }
+}
+
+resource "google_compute_firewall" "allow-backend-to-backend-egress" {
+  project   = google_project.project.project_id
+  name      = "allow-backend-to-backend-egress"
   network   = google_compute_network.vpc_network.name
   direction = "EGRESS"
 
@@ -54,20 +74,51 @@ resource "google_compute_firewall" "allow-backend-ping-backend-egress" {
     protocol = "icmp"
   }
 
-  priority = 1
+  priority = 60000
 }
 
-resource "google_compute_firewall" "allow-ssh" {
+resource "google_compute_firewall" "block-backend-egress" {
   project   = google_project.project.project_id
-  name      = "allow-ssh"
+  name      = "block-backend-egress"
+  network   = google_compute_network.vpc_network.name
+  direction = "EGRESS"
+
+  target_service_accounts = [google_service_account.backend.email]
+  destination_ranges      = ["0.0.0.0/0"]
+
+  deny {
+    protocol = "all"
+  }
+
+  priority = 65000
+}
+
+resource "google_compute_firewall" "allow-all-egress" {
+  project   = google_project.project.project_id
+  name      = "allow-all-egress"
+  network   = google_compute_network.vpc_network.name
+  direction = "EGRESS"
+
+  destination_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "all"
+  }
+
+  priority = 65535
+}
+
+resource "google_compute_firewall" "deny-all-ingress" {
+  project   = google_project.project.project_id
+  name      = "deny-all-ingress"
   network   = google_compute_network.vpc_network.name
   direction = "INGRESS"
 
-  target_tags   = ["open-ssh"]
   source_ranges = ["0.0.0.0/0"]
 
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
+  deny {
+    protocol = "all"
   }
+
+  priority = 65535
 }
